25329c580e4e92355442eb11d4f84986ca6454d7
Aktarul Ahsan <md.aktarualahsan@gmail.com>
2024-02-06 01:32:31 +0600

    fix

--- lib/presentation/task/task.screen.dart
+++ lib/presentation/task/task.screen.dart
@@ -8,12 +8,12 @@ class TaskScreen extends GetView<TaskController> {
   const TaskScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       // appBar: AppBar(
       //   title: const Text('TaskScreen'),
       //   centerTitle: true,
       // ),
-      body: const Center(
+      body: Center(
         child: Text(
           'TaskScreen is working',
           style: TextStyle(fontSize: 20),
